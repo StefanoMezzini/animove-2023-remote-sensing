0c1f6df51582df87625c419ef95c7ebe69bf84ea
Stefano Mezzini <42444204+StefanoMezzini@users.noreply.github.com>
2023-08-05 09:33:54 -0700

    added ud-based environmental variables

--- code/5-applying-to-movement-data.R
+++ code/5-applying-to-movement-data.R
@@ -1,3 +1,5 @@
+# example references: https://doi.org/10.1126/science.abo6499, https://doi.org/10.1186/s40462-022-00313-w
+
 library('dplyr')   # for data wrangling
 library('tidyr')   # for data wrangling
 library('purrr')   # for functional programming (fancy apply() functions)
@@ -12,7 +14,7 @@ theme_set(theme_bw())
 # import okanagan lake raster
 ok <- read_sf('data/okanagan-lake.shp')
 
-# import first NDVI rasters
+# import first NDVI raster
 r <- rast('data/modis-ndvi-rasters/kelowna-ndvi.tif')[[1]] %>%
   mask(ok, inverse = TRUE)
 
@@ -53,30 +55,88 @@ d <- mutate(d, ndvi = predict(m_ndvi, newdata = d, type = 'response'))
 plot(ndvi ~ timestamp, d)
 
 # do altitude and NDVI affect the animal's spatial needs?
-periods <- mutate(d,
-                  season = lubridate::quarter(timestamp), # ~ season
-                  individual.local.identifier = paste(year, season)) %>%
-  # group data by year and season (quarter)
-  nest(dataset = ! c(year, season)) %>%
-  mutate(
-    #' summarize the raster data (*information loss!*)
-    mean_ndvi = map_dbl(dataset, \(.d) mean(.d$ndvi)), # est. average NDVI
-    mean_elev = map_dbl(dataset, \(.d) mean(.d$elev)), # est. average elev.
-    # create a telemetry for each period
-    tel = map(dataset, as.telemetry),
-    # create variaograms for each period
-    vg = map(tel, \(.tel) ctmm.guess(.tel, interactive = FALSE)),
-    # select the best movement model for each period
-    ctmm = map2(tel, vg, \(.tel, .vg) ctmm.select(data = .tel, CTMM = .vg,
-                                                  trace = TRUE)),
-    # estimate the animal's UD
-    ud = map2(tel, ctmm, akde),
-    # extract the animal's 95% HR with CIs
-    hr_95_lwr = map_dbl(ud, \(.ud) summary(.ud)$CI[, 'low']),
-    hr_95_est = map_dbl(ud, \(.ud) summary(.ud)$CI[, 'est']),
-    hr_95_upr = map_dbl(ud, \(.ud) summary(.ud)$CI[, 'high']))
+if(! file.exists('models/movemen-models.rds')) {
+  periods <- mutate(d,
+                    quarter = lubridate::quarter(timestamp), # ~ season
+                    individual.local.identifier = paste(year, quarter)) %>%
+    # group data by year and quarter
+    nest(dataset = ! c(year, quarter)) %>%
+    mutate(
+      # summarize the raster data assuming uniform use
+      mean_ndvi = map_dbl(dataset, \(.d) mean(.d$ndvi)), # average NDVI
+      mean_elev = map_dbl(dataset, \(.d) mean(.d$elev)), # average elevation
+      # create a telemetry for each period
+      tel = map(dataset, as.telemetry),
+      # create variaograms for each period
+      vg = map(tel, \(.tel) ctmm.guess(.tel, interactive = FALSE)),
+      # select the best movement model for each period
+      ctmm = map2(tel, vg, \(.tel, .vg) ctmm.select(data = .tel, CTMM = .vg,
+                                                    trace = TRUE)),
+      # estimate the animal's UD
+      ud = map2(tel, ctmm, akde),
+      # extract the animal's 95% HR with CIs
+      hr_95_lwr = map_dbl(ud, \(.ud) summary(.ud)$CI[, 'low']),
+      hr_95_est = map_dbl(ud, \(.ud) summary(.ud)$CI[, 'est']),
+      hr_95_upr = map_dbl(ud, \(.ud) summary(.ud)$CI[, 'high']),
+      # calculate weighted averages using the UDs
+      ud_rast = map(ud, \(x) rast(raster(x, DF = 'PMF'))), # prob. mass fun.
+      wa_ndvi = map2_dbl(ud_rast, quarter, \(ud_r, q) {
+        new_dem <- project(dem, ud_r)
+        
+        left_join(as.data.frame(ud_r, xy = TRUE) %>%
+                    rename(pm = 3), # to avoid "layer" column name
+                  as.data.frame(new_dem, xy = TRUE) %>%
+                    rename(elev = 3), # needed for m_ndvi
+                  by = c('x', 'y')) %>%
+          rename(long = x, lat = y) %>%
+          mutate(doy = case_when(q == 1 ~ 45,
+                                 q == 2 ~ 136,
+                                 q == 3 ~ 227,
+                                 q == 4 ~ 319)) %>%
+          mutate(.,
+                 ndvi = predict(m_ndvi, newdata = ., type = 'response'),
+                 prod = ndvi * pm) %>%
+          pull(prod) %>%
+          sum(na.rm = TRUE) %>%
+          return()
+      }),
+      wa_elev = map_dbl(ud_rast, \(x) {
+        new_dem <- project(dem, x)
+        
+        left_join(as.data.frame(x, xy = TRUE) %>%
+                    rename(pm = 3),
+                  as.data.frame(new_dem, xy = TRUE) %>%
+                    rename(elev = 3),
+                  by = c('x', 'y')) %>%
+          mutate(prod = pm * elev) %>%
+          pull(prod) %>%
+          sum(na.rm = TRUE) %>%
+          return()
+      }))
+  
+  saveRDS(periods, 'models/movement-models.rds')
+} else {
+  pediods <- readRDS('models/movement-models.rds')
+}
 
-#' plot the results
+# diagnostic plots ----
+example_data <- left_join(as.data.frame(r, xy = TRUE) %>%
+                            rename(ndvi = 3),
+                          as.data.frame(project(dem, r), xy = TRUE) %>%
+                            rename(elev = 3),
+                          by = c('x', 'y'))
+
+# scatter plot with smooth
+ggplot(example_data, aes(ndvi, elev)) +
+  geom_point() +
+  geom_smooth(method = 'gam', formula = y ~ s(x))
+
+# hex plot
+ggplot(example_data) +
+  geom_hex(aes(ndvi, elev), bins = 10)
+
+# plot the results ----
+# averages based on tracking data
 ggplot(periods) +
   geom_point(aes(mean_ndvi, hr_95_est)) +
   geom_errorbar(aes(mean_ndvi, ymin = hr_95_lwr, ymax = hr_95_upr),
@@ -89,4 +149,19 @@ ggplot(periods) +
   geom_errorbar(aes(mean_elev, ymin = hr_95_lwr, ymax = hr_95_upr),
                 width = 0) +
   geom_smooth(aes(mean_elev, hr_95_est), method = 'glm', formula = y ~ x,
+              method.args = list(family = Gamma(link = 'log')))
+
+# ud-based averages
+ggplot(periods) +
+  geom_point(aes(wa_ndvi, hr_95_est)) +
+  geom_errorbar(aes(wa_ndvi, ymin = hr_95_lwr, ymax = hr_95_upr),
+                width = 0) +
+  geom_smooth(aes(wa_ndvi, hr_95_est), method = 'glm', formula = y ~ x,
+              method.args = list(family = Gamma(link = 'log')))
+
+ggplot(periods) +
+  geom_point(aes(wa_elev, hr_95_est)) +
+  geom_errorbar(aes(wa_elev, ymin = hr_95_lwr, ymax = hr_95_upr),
+                width = 0) +
+  geom_smooth(aes(wa_elev, hr_95_est), method = 'glm', formula = y ~ x,
               method.args = list(family = Gamma(link = 'log')))
